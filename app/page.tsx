e10fab6e39ed6270e3a792a7a47872c95e8907be
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-03-09 15:22:47 +0200

    implement active users

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { Live } from "@/components/live";
+import { Navbar } from "@/components/navbar";
 
 export default function Page() {
   return (
-    <div>
-      <Live />
-    </div>
+    <main className="h-screen overflow-hidden">
+      <Navbar />
+      <section className="flex h-full flex-row">
+        <Live />
+      </section>
+    </main>
   );
 }
