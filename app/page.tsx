927b72ccbeee068386ee6443e5eeda9f062b9299
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-05-06 12:41:13 +0300

    add left and right sidebars to the page layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@
 
 import { Live } from "@/components/live";
 import { Navbar } from "@/components/navbar";
+import { LeftSidebar } from "@/components/left-sidebar";
+import { RightSidebar } from "@/components/right-sidebar";
 
 export default function Page() {
   return (
     <main className="h-screen overflow-hidden">
       <Navbar />
       <section className="flex h-full flex-row">
+        <LeftSidebar />
         <Live />
+        <RightSidebar />
       </section>
     </main>
   );
